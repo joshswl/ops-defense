038234aca22d6009f39eed5a38af1f9d2f7e6cf6
Josh Lee <joshswl@gmail.com>
2017-11-17 17:24:19 -0600

    routes setup

--- app/router.js
+++ app/router.js
@@ -7,12 +7,13 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('about');
   this.route('mission');
   this.route('logistics');
   this.route('security');
   this.route('training');
   this.route('events');
+  this.route('contact');
+  this.route('home');
 });
 
 export default Router;
