bdf669232b5f310bd0a33dedc811a01cd58df8dc
Josh Lee <joshswl@gmail.com>
2017-11-17 17:26:17 -0600

    completely remove bootstrap addon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,11 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    'ember-bootstrap': {
-      'bootstrapVersion': 3,
-      'importBootstrapFont': true,
-      'importBootstrapCSS': false
-    }
+    // ember addon goes here
   });
 
   // Use `app.import` to add additional libraries to the generated
